a319616d5f65346909ed5acdd6aabef14fd69ae4
vpuzyrevich <v.puzyrevich@gmail.com>
2022-11-15 16:14:57 +0300

    feat: add a section about the team

--- src/service/translate.ts
+++ src/service/translate.ts
@@ -2,10 +2,24 @@ const i18Obj = {
   en: {
     signIn: 'Sign In',
     signUp: 'Sign Up',
+    titleWelcome: 'TaskManager',
+    descrWelcome:
+      "This is a program that helps manage tasks. Plan, manage and track all your team's tasks in one software.",
+    titleTeam: 'Our team',
+    nameV: 'Viktoria',
+    nameA: 'Asel',
+    nameD: 'Darya',
   },
   ru: {
     signIn: 'Вход',
     signUp: 'Регистриция',
+    titleWelcome: 'Таск-менеджер',
+    descrWelcome:
+      'Это программа, которая помогает управлять задачами. Планируйте, управляйте и отслеживайте все задачи вашей команды в одном программном обеспечении.',
+    titleTeam: 'Наша команда',
+    nameV: 'Виктория',
+    nameA: 'Асель',
+    nameD: 'Дарья',
   },
 };
 export default i18Obj;
