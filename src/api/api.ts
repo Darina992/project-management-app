59b36fb69702fcc1d1516a5ee672bc0c0ebfbb1b
Darina992 <darina992@gmail.com>
2022-11-18 16:43:07 +0300

    feat: add api for user update and delete

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,11 +1,11 @@
-import { apiPath, usersEndpoint } from './apiPath';
+import { apiPath, apiEndpoints } from './apiPath';
 import { IUser, INewUser, IToken } from './typesApi';
 import { getFromLocalStorage } from '../utils/utils';
 
 export const api = {
   async createNewUser(name: string, login: string, password: string): Promise<IUser | number> {
     try {
-      const response = await fetch('http://134.209.192.22:4000/signup', {
+      const response = await fetch(`${apiPath}${apiEndpoints.signup}`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
@@ -19,10 +19,8 @@ export const api = {
       });
       if (response.ok) {
         const data = await response.json();
-        window.location.href = '/main';
         return data;
       } else if (response.status === 409) {
-        console.log('Login is repeated');
         return response.status;
       } else {
         return await Promise.reject(new Error(response.statusText));
@@ -34,7 +32,7 @@ export const api = {
 
   async signInUser(login: string, password: string): Promise<IToken | number> {
     try {
-      const response = await fetch('http://134.209.192.22:4000/signin', {
+      const response = await fetch(`${apiPath}${apiEndpoints.signin}`, {
         method: 'POST',
         headers: {
           Accept: 'application/json',
@@ -49,13 +47,94 @@ export const api = {
         const data = await response.json();
         return data;
       } else if (response.status === 403) {
-        console.log('User not registered');
         return response.status;
       } else {
         return await Promise.reject(new Error(response.statusText));
       }
     } catch (error) {
       throw new Error('Authorization failed');
+    }
+  },
+  async getAllUsers(): Promise<IUser[]> {
+    try {
+      const response = await fetch(`${apiPath}${apiEndpoints.users}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${getFromLocalStorage('$token')}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        return data;
+      } else {
+        return await Promise.reject(new Error(response.statusText));
+      }
+    } catch (error) {
+      throw new Error('Failed get all users');
+    }
+  },
+  async getUserById(): Promise<IUser> {
+    try {
+      const response = await fetch(
+        `${apiPath}${apiEndpoints.users}${getFromLocalStorage('$userId')}`,
+        {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${getFromLocalStorage('$token')}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        return data;
+      } else {
+        return await Promise.reject(new Error(response.statusText));
+      }
+    } catch (error) {
+      throw new Error('Failed get user info');
+    }
+  },
+  async deleteUser() {
+    try {
+      const response = await fetch(
+        `${apiPath}${apiEndpoints.users}${getFromLocalStorage('$userId')}`,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${getFromLocalStorage('$token')}`,
+          },
+        }
+      );
+    } catch (error) {
+      throw new Error('User is not deleted');
+    }
+  },
+  async editUser(name: string, login: string, password: string): Promise<IUser | undefined> {
+    try {
+      const response = await fetch(
+        `${apiPath}${apiEndpoints.users}${getFromLocalStorage('$userId')}`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${getFromLocalStorage('$token')}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: name,
+            login: login,
+            password: password,
+          }),
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      }
+    } catch (error) {
+      throw new Error('User is not found');
     }
   },
 };
--- src/api/apiPath.ts
+++ src/api/apiPath.ts
@@ -1,2 +1,6 @@
 export const apiPath = 'http://134.209.192.22:4000/';
-export const usersEndpoint = 'users';
+export const apiEndpoints = {
+  users: 'users/',
+  signin: 'signin',
+  signup: 'signup',
+};
