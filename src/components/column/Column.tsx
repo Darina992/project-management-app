62e01fc525e964e7353ab06a54a81d4f9e85a08a
vpuzyrevich <v.puzyrevich@gmail.com>
2022-11-21 17:04:43 +0300

    feat: add column title edit

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Card,
   CardActions,
@@ -6,25 +7,60 @@ import {
   CardHeader,
   IconButton,
   TextField,
+  Typography,
 } from '@mui/material';
 import React, { useState } from 'react';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import AddIcon from '@mui/icons-material/Add';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import { useForm } from 'react-hook-form';
 
 export const Column = () => {
-  const [isChangeTitle, setIsChangeTitle] = useState(false);
+  const [isEditTitleColumn, setIsEditTitleColumn] = useState(false);
+  const [titleColumn, setTitleColumn] = useState('title');
+  const { register, handleSubmit, getValues } = useForm();
+
+  const onSaveTitleColumn = () => {
+    setTitleColumn(getValues('title'));
+    setIsEditTitleColumn(false);
+  };
+  const formTitleColumn = () => {
+    return (
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSaveTitleColumn)}
+        sx={{ mt: 1, display: 'flex' }}
+      >
+        <TextField defaultValue={titleColumn} size="small" {...register('title')} />
+        <Box sx={{ display: 'flex' }}>
+          <IconButton aria-label="settings" type="submit">
+            <CheckIcon color="success" fontSize="small" />
+          </IconButton>
+          <IconButton aria-label="settings" onClick={() => setIsEditTitleColumn(false)}>
+            <CloseIcon color="error" fontSize="small" />
+          </IconButton>
+        </Box>
+      </Box>
+    );
+  };
+
   return (
     <Card sx={{ width: 300 }}>
-      <CardHeader
-        title={isChangeTitle ? <TextField value="text" sx={{ outline: 'none' }} /> : 'title'}
-        action={
-          <IconButton aria-label="settings">
-            <DeleteOutlineIcon color="error" fontSize="small" />
-          </IconButton>
-        }
-        disableTypography
-        sx={{ fontSize: 22, fontFamily: 'Montserrat' }}
-      />
+      {isEditTitleColumn ? (
+        <CardHeader title={formTitleColumn()} />
+      ) : (
+        <CardHeader
+          title={<Typography onClick={() => setIsEditTitleColumn(true)}>{titleColumn}</Typography>}
+          action={
+            <IconButton aria-label="settings" onClick={() => console.log('delate')}>
+              <DeleteOutlineIcon fontSize="small" />
+            </IconButton>
+          }
+          disableTypography
+          sx={{ fontSize: 22, fontFamily: 'Montserrat' }}
+        />
+      )}
       <CardContent></CardContent>
       <CardActions>
         <Button variant="text" startIcon={<AddIcon />}>
