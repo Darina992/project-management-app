8ffd30dd02bf19f3e0069ec032a62a06d5ec5f9b
vpuzyrevich <v.puzyrevich@gmail.com>
2022-11-27 13:02:27 +0300

    fix: fix updating task list immediately after adding a new task

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,15 +1,20 @@
 import { Box, IconButton, Typography } from '@mui/material';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { ITask } from 'api/typesApi';
-import { deleteTask } from 'store/boardReducer';
+import { deleteTask, getBoardData } from 'store/boardReducer';
 import { useSelector } from 'react-redux';
 import { AppDispatch, RootState } from 'store';
 import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 export const Task: FC<{ taskData: ITask; columnId: string }> = ({ taskData, columnId }) => {
   const { boardData } = useSelector((state: RootState) => state.board);
   const dispatch = useDispatch<AppDispatch>();
+  const { idBoard } = useParams();
+  useEffect(() => {
+    dispatch(getBoardData(idBoard as string));
+  }, [dispatch, idBoard, columnId]);
   return (
     <Box
       sx={{
