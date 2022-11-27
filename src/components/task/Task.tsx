cce4b6ad1a9a5e2e9e0d348004a9cd88d42af7af
vpuzyrevich <v.puzyrevich@gmail.com>
2022-11-27 14:06:50 +0300

    feat: add delete tasks

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -2,19 +2,31 @@ import { Box, IconButton, Typography } from '@mui/material';
 import React, { FC, useEffect } from 'react';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { ITask } from 'api/typesApi';
-import { deleteTask, getBoardData } from 'store/boardReducer';
-import { useSelector } from 'react-redux';
-import { AppDispatch, RootState } from 'store';
+import { getBoardData } from 'store/boardReducer';
+import { AppDispatch } from 'store';
 import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { actionsOpenModal } from 'store/modalReducer';
 
 export const Task: FC<{ taskData: ITask; columnId: string }> = ({ taskData, columnId }) => {
-  const { boardData } = useSelector((state: RootState) => state.board);
   const dispatch = useDispatch<AppDispatch>();
   const { idBoard } = useParams();
+
   useEffect(() => {
     dispatch(getBoardData(idBoard as string));
   }, [dispatch, idBoard, columnId]);
+
+  const handleDelete = () => {
+    const data = {
+      id: taskData.id,
+      actionFor: 'task',
+    };
+    dispatch(actionsOpenModal.setDelteId(data));
+    dispatch(actionsOpenModal.setIdBoard(idBoard));
+    dispatch(actionsOpenModal.setIdColumn(columnId));
+    dispatch(actionsOpenModal.setOpenDilog(true));
+  };
+
   return (
     <Box
       sx={{
@@ -27,18 +39,7 @@ export const Task: FC<{ taskData: ITask; columnId: string }> = ({ taskData, colu
       }}
     >
       <Typography sx={{ p: 1 }}>{taskData.title}</Typography>
-      <IconButton
-        aria-label="settings"
-        onClick={() =>
-          dispatch(
-            deleteTask({
-              boardId: boardData?.id as string,
-              columnId: columnId,
-              taskId: taskData.id,
-            })
-          )
-        }
-      >
+      <IconButton aria-label="settings" onClick={() => handleDelete()}>
         <DeleteOutlineIcon fontSize="small" />
       </IconButton>
     </Box>
