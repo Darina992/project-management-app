0ab4c064e671eff782d11d90edbc8bc6f2d16cd6
vpuzyrevich <v.puzyrevich@gmail.com>
2022-12-05 09:25:19 +0300

    fix: fix conteiner for iconButton on maimPage

--- src/pages/mainPage/BoardRender.tsx
+++ src/pages/mainPage/BoardRender.tsx
@@ -1,18 +1,9 @@
-import {
-  Card,
-  CardActionArea,
-  CardActions,
-  CardContent,
-  Grid,
-  IconButton,
-  Typography,
-} from '@mui/material';
+import { Card, CardActions, CardContent, Grid, IconButton, Typography } from '@mui/material';
 import React, { FC, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ModeRoundedIcon from '@mui/icons-material/ModeRounded';
 import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
-import { green, pink } from '@mui/material/colors';
 import { AppDispatch, RootState } from 'store';
 import { getAllBoard } from 'store/mainReducer ';
 import { IBoard } from '../../types/boardsTypes';
@@ -36,7 +27,7 @@ export const BoardRender: FC<{ id: string; title: string; description: string }>
     dispatch(actionsOpenModal.setOpen(true));
   };
 
-  const handleClickOpen = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const handleClickOpen = (e: React.MouseEvent) => {
     const clases = e.currentTarget.classList;
     const data = {
       id: clases[1] as string,
@@ -49,27 +40,25 @@ export const BoardRender: FC<{ id: string; title: string; description: string }>
 
   return (
     <Grid item xs={12} sm={4} md={3}>
-      <Card>
-        <CardActionArea sx={{ backgroundColor: 'rgb(220, 228, 233)' }}>
-          <Link className="board__link" to={`/board/${id}`}>
-            <CardContent>
-              <Typography noWrap gutterBottom variant="h5" component="div">
-                {title}
-              </Typography>
-              <Typography noWrap variant="body2">
-                {description}
-              </Typography>
-            </CardContent>
-          </Link>
-          <CardActions>
-            <IconButton className={`button-icon ${id} chahge`} onClick={(e) => handleSubmit(e)}>
-              <ModeRoundedIcon />
-            </IconButton>
-            <div className={`button-icon ${id} delete`} onClick={(e) => handleClickOpen(e)}>
-              <DeleteRoundedIcon sx={{ color: 'rgb(205, 126, 120)' }} />
-            </div>
-          </CardActions>
-        </CardActionArea>
+      <Card sx={{ backgroundColor: 'rgb(220, 228, 233)' }}>
+        <Link className="board__link" to={`/board/${id}`}>
+          <CardContent>
+            <Typography noWrap gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography noWrap variant="body2">
+              {description}
+            </Typography>
+          </CardContent>
+        </Link>
+        <CardActions>
+          <IconButton className={`button-icon ${id} chahge`} onClick={(e) => handleSubmit(e)}>
+            <ModeRoundedIcon />
+          </IconButton>
+          <IconButton className={`button-icon ${id} delete`} onClick={(e) => handleClickOpen(e)}>
+            <DeleteRoundedIcon sx={{ color: 'rgb(205, 126, 120)' }} />
+          </IconButton>
+        </CardActions>
       </Card>
     </Grid>
   );
